33b62bccac2a761e8e78bcec5f5c85bd01934858
amanayumu <167833369+amanayumu@users.noreply.github.com>
2024-07-02 14:28:16 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,13 +1,6 @@
 alert('Hello,Ayumu!');
 alert(1+1);
 alert(Math.sqrt(1+1)) 
-
-et sqrt2 = Math.sqrt(2);
-let sqrt3 = Math.sqrt(3);
-let sum = sqrt2 + sqrt3;
-alert(sum);
-
-
 alert(2+3);
 alert('2'+'3');
 alert(Number('2')+Number('3'));
